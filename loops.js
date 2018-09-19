4a9a5d26717e847a30359f6cd7ad353ad983e914
Mary A <mary.alva@icloud.com>
2018-09-19 16:24:15 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,32 @@
+
+
+function forLoop(array) {
+  for (let i = 0; i<25; i++) {
+    if (i===1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push("I am " + i  + " strange loops.")
+    }
+  }
+  return array
+}
+
+
+function whileLoop(n) {
+  while (whileLoop > 0) {
+    console.log(--number);
+  }
+}
+
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+
+function doWhileLoop(array) {
+  do {
+   array - number
+  } while  array.length > 0 && incrementVariable();
+  return array
+}
