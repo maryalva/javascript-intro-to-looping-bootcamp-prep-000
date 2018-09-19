b4d61da03f2b5a39fa5dd4e2d7beeb29f250d57e
Mary A <mary.alva@icloud.com>
2018-09-19 20:07:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,7 +22,7 @@ function whileLoop(number) {
 
 function doWhileLoop(array) {
   do {
-    ;
+    console.log(array.pull());
   } while (array.length > 0 && incrementVariable());
   return array;
 }
